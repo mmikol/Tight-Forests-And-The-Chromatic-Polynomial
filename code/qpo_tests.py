82a3d5ff0783a0b88733d34ce03081a02e62a29b
Miliano Mikol <m.mikol@icloud.com>
2020-10-16 21:56:09 -0500

    Commit current tests

--- code/qpo_tests.py
+++ code/qpo_tests.py
@@ -23,7 +23,7 @@ class PathTraversalGeneratorTests(unittest.TestCase):
         specimen = Graph({1: [2]})
         traversals = set(generate_path_traversals(specimen))
         self.assertTrue(len(list(traversals)) == len({(1, 2), (2, 1)}))
-        self.assertTrue(all(p in traversals for p in EXPECTED))
+        self.assertTrue(all(sequence in traversals for sequence in EXPECTED))
         return
 
     def test_path_traversal_4(self):
@@ -36,7 +36,7 @@ class PathTraversalGeneratorTests(unittest.TestCase):
         specimen = Graph({1: [2, 3], 2: [3], 3: [4]})
         traversals = set(generate_path_traversals(specimen))
         self.assertTrue(len(traversals) == len(EXPECTED))
-        self.assertTrue(all(p in traversals for p in EXPECTED))
+        self.assertTrue(all(sequence in traversals for sequence in EXPECTED))
         return
 
 
@@ -54,10 +54,10 @@ class CandidatePathCheckerTests(unittest.TestCase):
 
     def test_is_candidate_path_3(self):
         POSSIBLE_CANDIDATE_PATHS = {(1, 4, 3, 2), (1, 4, 2, 3), (2, 4, 3, 1)}
-        for p in TestHelpers.generate_paths(vertex_labels=(1, 2, 3, 4), min_path_len=4, max_path_len=4):
-            p = tuple(p)
-            self.assertTrue(is_candidate_path(p) ==
-                            (p in POSSIBLE_CANDIDATE_PATHS))
+        for path in TestHelpers.generate_paths(vertex_labels=(1, 2, 3, 4), min_path_len=4, max_path_len=4):
+            path = tuple(path)
+            self.assertTrue(is_candidate_path(path) ==
+                            (path in POSSIBLE_CANDIDATE_PATHS))
         return
 
     def test_is_candidate_path_4(self):
@@ -70,14 +70,14 @@ class CandidatePathCheckerTests(unittest.TestCase):
             (1, 4, 2, 5, 3), (1, 4, 3, 5, 2), (2, 4, 3, 5, 1)
         }
 
-        for p in TestHelpers.generate_paths((1, 2, 3, 4, 5), min_path_len=4, max_path_len=5):
-            self.assertTrue(is_candidate_path(p) ==
-                            (p in POSSIBLE_CANDIDATE_PATHS))
+        for path in TestHelpers.generate_paths((1, 2, 3, 4, 5), min_path_len=4, max_path_len=5):
+            self.assertTrue(is_candidate_path(path) ==
+                            (path in POSSIBLE_CANDIDATE_PATHS))
 
         return
 
 
-"""class CandidatePathGeneratorTests(unittest.TestCase):
+class CandidatePathGeneratorTests(unittest.TestCase):
     def test_candidate_path_generator_1(self):
         specimen = Graph()
         candidate_paths = generate_candidate_paths(specimen)
@@ -97,20 +97,20 @@ class CandidatePathCheckerTests(unittest.TestCase):
         return
 
     def test_candidate_path_generator_4(self):
-        specimen = Graph({1: [2], 2: [3]})
-        candidate_paths = generate_candidate_paths(specimen)
-        self.assertTrue(not list(candidate_paths))
+        EXPECTED = (1, 4, 2, 3)
+        specimen = Graph({1: [4], 2: [3], 3: [1], 4: [2]})
+        candidate_paths = list(generate_candidate_paths(specimen))
+        self.assertTrue((len(candidate_paths) == 1)
+                        and candidate_paths == [EXPECTED])
         return
 
     def test_candidate_path_generator_5(self):
         POSSIBLE_CANDIDATE_PATHS = {(1, 4, 3, 2), (1, 4, 2, 3), (2, 4, 3, 1)}
-        vertices = [i + 1 for i in range(4)]
-        for a, b, c, d in Arrangements(vertices, len(vertices)):
+        vertex_labels = (1, 2, 3, 4)
+        for a, b, c, d in Arrangements(vertex_labels, len(vertex_labels)):
             specimen = Graph({a: [b], b: [c], c: [d]})
-            candidate_paths = generate_candidate_paths(specimen)
-            if list(candidate_paths):
-                self.assertTrue(all(p in POSSIBLE_CANDIDATE_PATHS
-                                    for p in candidate_paths))
+            for path in generate_candidate_paths(specimen):
+                self.assertTrue(path in POSSIBLE_CANDIDATE_PATHS)
         return
 
     def test_candidate_path_generator_6(self):
@@ -121,17 +121,15 @@ class CandidatePathCheckerTests(unittest.TestCase):
             candidate_paths = generate_candidate_paths(specimen)
             if list(candidate_paths):
                 self.assertTrue(
-                    all(p in POSSIBLE_CANDIDATE_PATHS for p in candidate_paths))
+                    all(cp in POSSIBLE_CANDIDATE_PATHS for cp in candidate_paths))
         return
 
     def test_candidate_path_generator_7(self):
         POSSIBLE_CANDIDATE_PATHS = {(2, 5, 3, 4), (1, 4, 3, 5, 2)}
         specimen = Graph({1: [4], 2: [1], 3: [5], 4: [3], 5: [2]})
-        candidate_paths = generate_candidate_paths(specimen)
-        print(list(candidate_paths))
-        self.assertTrue(
-            all(p in candidate_paths for p in POSSIBLE_CANDIDATE_PATHS))
+        self.assertTrue(all(path in generate_candidate_paths(specimen)
+                            for path in POSSIBLE_CANDIDATE_PATHS))
         return
-"""
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
