dbf9407ad10c3fb553ccf5749553c0e74fb9ff0a
Miliano Mikol <m.mikol@icloud.com>
2020-10-30 23:01:29 -0500

    Need to clean up a lot

--- code/QPO.py
+++ code/QPO.py
@@ -1,78 +1,41 @@
 import sage.all
-from itertools import combinations
 from sage.combinat.permutation import Arrangements
 from datetime import datetime
 from orderedset import OrderedSet
 
+# TODO This needs to really be cleaned up
 
-# TODO: create a better algorithm
+
 def candidate_paths(G):
     """Yields all candidate paths in a given graph"""
-    def backtrack(P, paths, current_vertex, path_position):
-        neighbors = set(G.neighbors(current_vertex)) - P
+    def _backtrack(path, current_vertex, path_position):
+        neighbors = set(G.neighbors(current_vertex)) - path
 
-        if path_position >= 3 and current_vertex < P[1]:
-            P.add(current_vertex)
-            paths.add(tuple(P))
-            return
+        if path_position >= 3 and current_vertex < path[1]:
+            path.add(current_vertex)
+            yield tuple(path)
 
-        valid_recurse = (
-            (path_position == 1 and current_vertex > P[0]) or
-            (path_position == 2 and current_vertex > P[0] and current_vertex < P[1]) or
-            (path_position >= 3 and current_vertex > P[1])
-        )
+        if ((path_position == 1 and current_vertex > path[0]) or
+            (path_position == 2 and current_vertex > path[0] and current_vertex < path[1]) or
+                (path_position >= 3 and current_vertex > path[1])):
 
-        if valid_recurse:
-            P.add(current_vertex)
+            path.add(current_vertex)
 
             for neighbor in neighbors:
-                backtrack(P, paths, neighbor, path_position + 1)
+                yield from _backtrack(path, neighbor, path_position + 1)
 
-        return
-
-    paths = set()
-    # Possible heuristics (since ordered, we know that paths can't begin with n largest)
-    for vertex in G.vertices():
-        # Possible heuristics (since ordered, we know that second vertex can't be with n smallest)
+    for vertex in G.vertex_iterator():
         for neighbor in G.neighbor_iterator(vertex):
-            backtrack(
-                P=OrderedSet([vertex]),
-                paths=paths,
+            yield from _backtrack(
+                path=OrderedSet([vertex]),
                 current_vertex=neighbor,
                 path_position=1)
-
-    return paths
 
 
 def permuted_graphs(G):
     for vertex_ordering in Arrangements(G.vertices(), G.order()):
         G.relabel(vertex_ordering)
         yield G
-
-
-def is_candidate_path(P):
-    """
-    Returns whether a path is of the form a - c - b - v_i - v_m = d
-    such that a < b < c and v_m is the only v_i smaller than c.
-    """
-
-    if (len(set(P)) != len(P)):
-        raise ValueError('path must be a sequence of distinct vertices')
-
-    MIN_LEN = 4
-
-    if len(P) < MIN_LEN:
-        return False
-
-    a, c, b, d = *P[:3], P[-1]
-
-    if not (a < b and b < c and d < c):
-        return False
-
-    if len(P) > MIN_LEN and any(v_i < c for v_i in P[MIN_LEN - 1:-1]):
-        return False
-
-    return True
 
 
 def has_QPO(G, show_checks=False):
