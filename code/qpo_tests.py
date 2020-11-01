5ea83b09b65944b19c9b0f40b3543d27f59cb35b
Miliano Mikol <m.mikol@icloud.com>
2020-10-31 22:11:18 -0500

    Update qpo_tests.py

--- code/qpo_tests.py
+++ code/qpo_tests.py
@@ -44,9 +44,6 @@ class CandidatePathGeneratorTests(unittest.TestCase):
             # All found paths are candidate paths
             self.assertTrue(
                 all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
-            # All candidate paths found
-            self.assertTrue(
-                all(path in specimen_candidate_paths for path in POSSIBLE_CANDIDATE_PATHS))
 
     def test_three_cycle_with_dangle(self):
         POSSIBLE_CANDIDATE_PATHS = {(1, 4, 3, 2), (1, 4, 2, 3), (2, 4, 3, 1)}
@@ -58,32 +55,29 @@ class CandidatePathGeneratorTests(unittest.TestCase):
                 # All found paths are candidate paths
                 self.assertTrue(
                     all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
-                # All candidate paths found
-                self.assertTrue(
-                    all(path in specimen_candidate_paths for path in POSSIBLE_CANDIDATE_PATHS))
 
-    def test_single_five_cycle(self):
-        POSSIBLE_CANDIDATE_PATHS = {(2, 5, 3, 4), (1, 4, 3, 5, 2)}
-        specimen = Graph({1: [4], 2: [1, 5], 3: [5], 4: [3]})
-        specimen_candidate_paths = set(candidate_paths(specimen))
-
-        # All found paths are candidate paths
-        self.assertTrue(
-            all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
-
-        # All candidate paths found
-        self.assertTrue(
-            all(path in specimen_candidate_paths for path in POSSIBLE_CANDIDATE_PATHS))
+    def test_all_five_cycles(self):
+        POSSIBLE_CANDIDATE_PATHS = {(1, 5, 2, 3), (1, 5, 3, 2), (1, 5, 3, 4),
+                                    (1, 5, 2, 4), (1, 5, 4, 2), (1, 5, 4, 3),
+                                    (1, 4, 2, 3), (1, 4, 3, 2), (1, 4, 3, 5, 2),
+                                    (1, 4, 2, 5, 3), (2, 5, 3, 4), (2, 5, 3, 1),
+                                    (2, 5, 4, 1), (2, 5, 4, 3), (2, 4, 3, 1),
+                                    (2, 4, 3, 5, 1), (3, 5, 4, 2), (3, 5, 4, 1)}
+        vertices = [i + 1 for i in range(5)]
+        for a, b, c, d, e in Arrangements(vertices, len(vertices)):
+            specimen = Graph({a: [b, e], b: [c], c: [d], d: [e]})
+            specimen_candidate_paths = set(candidate_paths(specimen))
+            # All found paths are candidate paths
+            self.assertTrue(
+                all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
 
     def test_K23_graph(self):
-        POSSIBLE_CANDIDATE_PATHS = {(3, 5, 4, 1), (2, 5, 3, 1)}
+        POSSIBLE_CANDIDATE_PATHS = {(3, 5, 4, 1), (2, 5, 3, 1), (2, 5, 4, 1)}
         specimen = Graph({1: [2, 3, 4], 5: [2, 3, 4]})
         specimen_candidate_paths = set(candidate_paths(specimen))
-
         # All found paths are candidate paths
         self.assertTrue(
             all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
-
         # All candidate paths found
         self.assertTrue(
             all(path in specimen_candidate_paths for path in POSSIBLE_CANDIDATE_PATHS))
@@ -92,11 +86,9 @@ class CandidatePathGeneratorTests(unittest.TestCase):
         POSSIBLE_CANDIDATE_PATHS = {(1, 4, 3, 2), (1, 7, 6, 5)}
         specimen = Graph({1: [2, 4, 5, 7], 3: [2, 4], 6: [5, 7]})
         specimen_candidate_paths = set(candidate_paths(specimen))
-
         # All found paths are candidate paths
         self.assertTrue(
             all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
-
         # All candidate paths found
         self.assertTrue(
             all(path in specimen_candidate_paths for path in POSSIBLE_CANDIDATE_PATHS))
@@ -105,43 +97,25 @@ class CandidatePathGeneratorTests(unittest.TestCase):
         POSSIBLE_CANDIDATE_PATHS = {(1, 5, 3, 2), (2, 6, 4, 1)}
         specimen = Graph({1: [2, 4, 5], 2: [3, 6], 3: [5], 4: [6]})
         specimen_candidate_paths = set(candidate_paths(specimen))
-
-        # All foundpaths are candidate paths
+        # All found paths are candidate paths
         self.assertTrue(
             all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
-
         # All candidate paths found
         self.assertTrue(
             all(path in specimen_candidate_paths for path in POSSIBLE_CANDIDATE_PATHS))
 
-    def test_complex_graph(self):
-        POSSIBLE_CANDIDATE_PATHS = {
-            (1, 10, 9, 4),
-            (1, 10, 9, 8),
-            (3, 7, 5, 2),
-            (3, 7, 5, 6),
-            (3, 7, 5, 11, 6),
-            (3, 7, 5, 8, 9, 4),
-            (3, 7, 5, 8, 9, 10, 1),
-            (4, 9, 8, 5)
-        }
-        specimen = Graph({
-            1: [4, 10],
-            2: [5],
-            3: [7],
-            4: [1, 9],
-            5: [2, 7, 8, 11, 6],
-            6: [5, 11],
-            7: [3, 5],
-            8: [5, 9],
-            9: [4, 8, 10],
-            10: [1, 9],
-            11: [5, 6]
-        })
+    def test_random_complex_graph(self):
+        POSSIBLE_CANDIDATE_PATHS = {(1, 10, 9, 4), (1, 10, 9, 8), (3, 7, 5, 2),
+                                    (3, 7, 5, 6), (3, 7, 5, 11,
+                                                   6), (3, 7, 5, 8, 9, 4),
+                                    (3, 7, 5, 8, 9, 10, 1), (4, 9, 8, 5)}
+        specimen = Graph({1: [4, 10], 5: [2, 7, 8, 11, 6],
+                          7: [3], 9: [4, 8, 10], 11: [6]})
         specimen_candidate_paths = set(candidate_paths(specimen))
-        print(list(candidate_paths(specimen)))
+        # All found paths are candidate paths
         self.assertTrue(
             all(path in POSSIBLE_CANDIDATE_PATHS for path in specimen_candidate_paths))
+        # All candidate paths found
         self.assertTrue(
             all(path in specimen_candidate_paths for path in POSSIBLE_CANDIDATE_PATHS))
 
