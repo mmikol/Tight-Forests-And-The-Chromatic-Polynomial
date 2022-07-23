a6c675e29b05a1cbc4821d29a0fd9ba6f0fa6a1d
Miliano Mikol <33736581+mmikol@users.noreply.github.com>
2022-07-22 17:33:20 -0700

    Working on iterative solution

--- code/QPO.py
+++ code/QPO.py
@@ -1,54 +1,66 @@
 from sage.all import *
 from sage.combinat.permutation import Arrangements
-from datetime import datetime
-from ordered_set import OrderedSet
+# from datetime import datetime
 
-
-def last_possible_vertex(path, path_position, current_vertex):
-    # For a-c-b-...-v_i-...-v_m = d, d < c
-    return path_position >= 3 and current_vertex < path[1]
-
-
-def still_candidate_path(path, current_vertex, path_position):
-    # For a-c, a < c
-    if path_position == 1:
-        return current_vertex > path[0]
-
-    # For a-c-b, a < b < c
-    if path_position == 2:
-        return current_vertex > path[0] and current_vertex < path[1]
-
-    # For a-c-b-...-v_i-..., v_i > c
-    if path_position >= 3:
-        return current_vertex > path[1]
-
+def valid_candidate_path(path, v):
+    if len(path) == 1:            
+        return path[0] < v
+    if len(path) == 2:
+        return path[0] < v < path[1]
+    if len(path) >= 3:
+        return path[1] < v
     return False
 
+def candidate_paths(G, v): 
+    stack = [[v]]
+    while len(stack) > 0:
+        path = stack.pop()
+        for n in filter(lambda n: n not in path, G.neighbors(path[-1])):
+            if len(path) >= 3 and n < path[1]:
+                yield (*path , n)
+            if valid_candidate_path(path , n):
+                stack.append([*path , n])
+                
+# an iterative solution TODO: test and prove complexity/completeness
+def is_QPO(G):
+    for v in G.vertices():
+        for p in candidate_paths(G, v):
+            a, c, b, d = *p[:3], p[-1]
+            if (not G.has_edge(a, d) and not (d < b and G.has_edge(c, d))):
+                return False, p
+    return True, ()
 
-def candidate_paths(G):
-    """Yields all candidate paths in a given graph"""
-
+def recursive_candidate_paths(G):
+    def valid_candidate_path(path, current_vertex, path_position):
+        if path_position == 1:
+            return path[0] < current_vertex
+        if path_position == 2:
+            return path[0] < current_vertex < path[1]
+        if path_position >= 3:
+            return path[1] < current_vertex
+        return False
+    
     def search(path, current_vertex, path_position):
-        neighbors = set(G.neighbors(current_vertex)) - path
-
-        if last_possible_vertex(path, path_position, current_vertex):
+        if path_position >= 3 and current_vertex < path[1]:
             yield tuple([*path, current_vertex])
-
-        if still_candidate_path(path, current_vertex, path_position):
-            path.add(current_vertex)
-            for neighbor in neighbors:
+        if valid_candidate_path(list(path), current_vertex, path_position):
+            path.append(current_vertex)
+            for neighbor in filter(lambda n: n not in path, G.neighbors(current_vertex)):
                 yield from search(path, neighbor, path_position + 1)
             path.remove(current_vertex)
-
+            
     # Property: Two largest vertices never begin a candidate path
-    for vertex in G.vertices()[: G.order() - 1]:
+    for vertex in G.vertices()[:G.order() - 1]:
         # Property: Three smallest vertices never follow the first vertex in a candidate path
-        neighbors = filter(lambda v: G.order() > 3 and v > G.vertices()[2],
-                           G.neighbors(vertex))
-        for neighbor in neighbors:
-            yield from search(path=OrderedSet([vertex]), current_vertex=neighbor, path_position=1)
+        for neighbor in filter(lambda v: G.order() > 3 and v > G.vertices()[2], G.neighbors(vertex)):
+            yield from search(path=[vertex], current_vertex=neighbor, path_position=1)
 
-
+def recursive_is_QPO(G):
+    for candidate_path in recursive_candidate_paths(G):
+        a, c, b, d = *candidate_path[:3], candidate_path[-1]
+        if (not G.has_edge(a, d) and not (d < b and G.has_edge(c, d))):
+            return False, candidate_path
+    return True, ()
 def labeled_graph_permutations(G):
     """
     Yields graphs with labels permuted according to a 
@@ -58,19 +70,6 @@ def labeled_graph_permutations(G):
         G.relabel(vertex_ordering)
         yield G
 
-
-def is_qpo(G):
-    """
-    Returns whether all candidate paths a-c-b-v_i-v_m=d
-    satisfy the condition that either a-d is an edge
-    or else d < b and c-d is an edge.
-    """
-    for candidate_path in candidate_paths(G):
-        a, c, b, d = *candidate_path[:3], candidate_path[-1]
-        if (not G.has_edge(a, d) and not (d < b and G.has_edge(c, d))):
-            return False, candidate_path
-
-    return True, ()
 
 
 # def qpo_check(G, show_checks=False):
